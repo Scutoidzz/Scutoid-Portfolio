6b05f9aa86972659c0467470cc7e698a9cdd8c1b
Sterling Duncan <techedreal@gmail.com>
2025-12-27 16:53:25 -0600

    feat: introduce blog post fetching script, refactor contact page UI, and refine project button styling.

---
+++ httpdocs/getposts.js
@@ -0,0 +1,10 @@
+function getPosts() {
+    fetch('blogposts.sql')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
