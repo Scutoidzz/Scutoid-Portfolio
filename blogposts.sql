615670435a73b81021182c36e4c7a213bdc5105b
Sterling Duncan <techedreal@gmail.com>
2025-12-27 11:39:38 -0600

    first commit

---
+++ blogposts.sql
@@ -0,0 +1,8 @@
+CREATE TABLE blogposts (
+    id INT PRIMARY KEY,
+    title VARCHAR(255),
+    content TEXT,
+    author VARCHAR(255),
+    date DATE
+);
+
---
+++ scripts.js
@@ -0,0 +1,41 @@
+function open_js() {
+    window.open("https://github.com/Scutoidzz?tab=repositories&q=&type=&language=javascript&sort=")
+}
+
+function open_html() {
+    window.open("https://github.com/Scutoidzz?tab=repositories&q=&type=&language=html&sort=")
+}
+
+function open_python() {
+    window.open("https://github.com/Scutoidzz?tab=repositories&q=&type=&language=python&sort=")
+}
+
+function open_css() {
+    window.open("https://github.com/Scutoidzz?tab=repositories&q=&type=&language=css&sort=")
+}
+
+
+function open_home() {
+    window.open("index.html")
+}
+
+function open_projects() {
+    window.open("projects.html")
+}
+
+function open_blog() {
+    window.open("blog.html")
+}
+
+function open_contact() {
+    window.open("contact.html")
+}
+
+function open_email() {
+    window.open("mailto:support@scutoid.top")
+}
+
+
+function open_github() {
+    window.open("https://github.com/Scutoidzz")
+}
